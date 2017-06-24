16bedab36462bce4f0a668bdf064b4641cae3121
Druotic <jamesjbeavers@gmail.com>
2017-06-24 18:17:23 -0400

    [NO-ISSUE] ES6ify

--- .githooks/commit-msg/branch-name-commit-modifier.js
+++ .githooks/commit-msg/branch-name-commit-modifier.js
@@ -4,44 +4,44 @@
 'use strict';
 
 require('require-self-ref')
-var childProcess = require('child_process');
-var fs = require('fs');
+const childProcess = require('child_process');
+const fs = require('fs');
 
 function main (options) {
     if (!options) {
         return;
     }
-    var commitMessagePath = options.commitPath;
+    const commitMessagePath = options.commitPath;
 
-    var preChar = options.preChar || '[';
-    var postChar = options.postChar || ']';
-    var delimChar = options.delimiter || '-';
+    const preChar = options.preChar || '[';
+    const postChar = options.postChar || ']';
+    const delimChar = options.delimiter || '-';
 
-    var projectName = options.projectName || '[A-Z]+';
+    const projectName = options.projectName || '[A-Z]+';
 
-    var regExpString = '^\\' + preChar + '?';
-    regExpString += '(' + projectName + delimChar;
-    regExpString += '[0-9]+\\' + postChar + '?' + ')';
+    let regExpString = `^\\${preChar}?`;
+    regExpString += `(${projectName}${delimChar}`;
+    regExpString += `[0-9]+\\${postChar}?)`;
 
-    var detectRegExp = new RegExp(regExpString);
+    const detectRegExp = new RegExp(regExpString);
 
-    var gitBranch = childProcess.execSync('git status --porcelain --long');
+    let gitBranch = childProcess.execSync('git status --porcelain --long');
     gitBranch = gitBranch.toString().split('\n')[0].substring(10);
-    var gitBranchPrefix = detectRegExp.exec(gitBranch);
+    let gitBranchPrefix = detectRegExp.exec(gitBranch);
     gitBranchPrefix = gitBranchPrefix ? gitBranchPrefix[0] : null;
 
-    var commitMessage = fs.readFileSync(commitMessagePath, 'utf8');
-    var commitBranch = detectRegExp.exec(commitMessage);
+    const commitMessage = fs.readFileSync(commitMessagePath, 'utf8');
+    let commitBranch = detectRegExp.exec(commitMessage);
     commitBranch = commitBranch ? commitBranch[0] : null;
 
-    var newCommitMessage;
+    let newCommitMessage;
 
     if (!commitBranch && !gitBranchPrefix) {
         console.log('Issue prefix not specified in commit and not on issue labeled branch, prepending NO-ISSUE...');
-        newCommitMessage = preChar + 'NO-ISSUE' + postChar + ' ' + commitMessage;
+        newCommitMessage = `${preChar}NO-ISSUE${postChar} ${commitMessage}`;
     } else if (!commitBranch){
         console.log('Issue prefix not specified in commit, prepending branch issue prefix...');
-        newCommitMessage = preChar + gitBranchPrefix + postChar + ' ' + commitMessage;
+        newCommitMessage = `${preChar}${gitBranchPrefix}${postChar} ${commitMessage}`;
     } else {
         console.log('Issue prefix present in commit message, ignoring...');
         newCommitMessage = commitMessage;
@@ -50,7 +50,7 @@ function main (options) {
 }
 
 function getConfig () {
-    var config = require('~/package.json').config.branchNameCommitModifier;
+    const config = require('~/package.json').config.branchNameCommitModifier;
     config.commitPath = process.argv[2];
     main(config);
 }
