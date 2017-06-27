526da97d53f4bc75d72443ed6326464da56a79db
Charlie Duong <charlieduong94@gmail.com>
2017-06-27 19:36:00 -0400

    [WB-32] Use queue util's createManagedConsumer

--- src/consumers/index.js
+++ src/consumers/index.js
@@ -1,6 +1,6 @@
 const config = require('~/src/config')
 const logger = require('~/src/logging').logger(module)
-const ManagedConsumer = require('windbreaker-service-util/queue/ManagedConsumer')
+const queueUtil = require('windbreaker-service-util/queue')
 
 const EVENTS_QUEUE_NAME = config.getEventsQueueName()
 const EVENTS_QUEUE_PREFETCH_COUNT = config.getEventsQueuePrefetchCount()
@@ -11,7 +11,8 @@ const CONSUMER_RECONNECT_TIMEOUT = config.getConsumerReconnectTimeout()
 exports.initialize = async function () {
   const amqUrl = config.getAmqUrl()
 
-  const eventsConsumer = new ManagedConsumer({
+  // returns a managed consumer instance
+  await queueUtil.createManagedConsumer({
     amqUrl,
     logger,
     restart: true,
@@ -26,7 +27,7 @@ exports.initialize = async function () {
     }
   })
 
-  const workConsumer = new ManagedConsumer({
+  await queueUtil.createManagedConsumer({
     amqUrl,
     logger,
     restart: true,
@@ -42,19 +43,13 @@ exports.initialize = async function () {
   })
 
   // In the future, we may want fine control over the consumer instances
-  // eventsConsumer.on('consumer-restarted', (simpleConsumer) => {
+  // managedConsumer.on('consumer-restarted', (simpleConsumer) => {
   //  // handle new consumer
   // })
   //
   // we may also handle situations where we fail to create consumers
   // (possibly kill process and allow server to reset)
-  // eventsConsumer.on('consumer-restart-failed', (err) => {
+  // managedConsumer.on('consumer-restart-failed', (err) => {
   //  // handle restart failure
   // })
-
-  // Create events queue consumer
-  await eventsConsumer.start()
-
-  // Create events queue consumer
-  await workConsumer.start()
 }
